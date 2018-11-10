19d7518d88efa86b87de48991e5ea39aa82220bf
haibowen <1461154748@qq.com>
2018-11-10 20:45:30 +0800

    算法练习

--- first/src/com/haibowen/sort/Merage.java
+++ first/src/com/haibowen/sort/Merage.java
@@ -1,4 +1,71 @@
 package com.haibowen.sort;
 
+import java.util.Arrays;
+
 public class Merage {
+    private  static  Comparable[]aux;
+
+    public  static  void  sort(Comparable[]a){
+        aux=new Comparable[a.length];
+        sort(a,0,a.length-1);
+
+    }
+    private static  void  sort(Comparable[]a,int lo,int hi){
+
+        if (hi<=lo)return;
+        int mid=lo+(hi-lo)/2;
+        sort(a,lo,mid);
+        sort(a,mid+1,hi);
+        merge(a,lo,mid,hi);
+
+
+    }
+
+
+
+
+    public  static  void  merge(Comparable []a ,int lo,int mid,int hi){
+
+
+        int i=lo;
+        int j=mid+1;
+        Comparable [] aux = new Comparable[hi+1];
+        for ( int k=lo;k<=hi;k++){
+          aux[k]=a[k];
+
+
+        }
+
+        for (int k=lo;k<=hi;k++){
+
+            if (i>mid){
+                a[k]=aux[j++];
+            }else if (j>hi){
+                a[k]=aux[i++];
+            }else if (less(aux[j],aux[i])){
+
+
+                a[k]=aux[j++];
+            }else {
+                a[k]=aux[i++];
+            }
+        }
+
+
+    }
+
+
+    public  static  boolean  less(Comparable v,Comparable w){
+
+        return v.compareTo(w)<0;
+    }
+
+    public static void main(String[] args) {
+
+
+        Comparable [] a={3,2,6,3,5,89,345,123,345,67,3456,1,22,33,16};
+        sort(a,0,14);
+        System.out.println(Arrays.toString(a));
+    }
+
 }
---
+++ first/src/com/haibowen/sort/Quick.java
@@ -0,0 +1,55 @@
+package com.haibowen.sort;
+
+import edu.princeton.cs.algs4.StdRandom;
+
+public class Quick {
+
+    public  static  void  sort(Comparable []a){
+
+        StdRandom.shuffle(a);
+        sort(a,0,a.length-1);
+
+    }
+    private  static void  sort(Comparable[]a,int lo,int hi){
+        if (hi<=lo)return;
+        int j=partition(a,lo,hi);
+        sort(a,lo,j-1);
+        sort(a,j+1,hi);
+
+
+    }
+    private static  int partition(Comparable[]a,int lo,int hi){
+
+        int i=lo;
+        int j=hi+1;
+        Comparable v=a[lo];
+        while (true)
+        {
+
+            while (less(a[++i],v))
+                if (i==hi)
+                    break;
+                while (less(v,a[--j]))
+                    if (j==lo)
+                        break;
+                    if (i>=j)
+                        break;
+                    exach(a,j,j);
+
+        }
+
+        exach(a,lo,j);
+        return j;
+    }
+    public  static  boolean less(Comparable v,Comparable w){
+
+        return v.compareTo(w)<0;
+    }
+    public static  void  exach(Comparable[] a,int i,int j){
+
+        Comparable t=a[i];
+        a[i]=a[j];
+        a[j]=t;
+
+    }
+}
